a04c4295213646e3654d668244cfa99fc9f6d1ec
Samuel Mzee <samuelmzeee40@gmail.com>
2021-02-21 23:29:16 -0600

    Added some transition elements to the images in the footer

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,21 @@
-console.log("Hello world");
+  $(document).ready(function(){
+    $("#fb").hover(function(){
+        $('#fb').attr('src','images/mf.png');
+      }, function(){
+        $('#fb').attr('src','images/Facebook.png');
+    });
+    
+    $("#twit").hover(function(){
+        $('#twit').attr('src','images/mt.png');
+      }, function(){
+        $('#twit').attr('src','images/Twitter.png');
+    });
+
+    $("#insta").hover(function(){
+        $('#insta').attr('src','images/mi.png');
+      }, function(){
+        $('#insta').attr('src','images/Instagram.png');
+    });
+
+  });
+
